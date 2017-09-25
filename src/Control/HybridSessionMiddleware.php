3e423f508edf4f12e079b1118ef379a3b0470de8
Will Rossiter <will@fullscreen.io>
2017-09-26 09:35:51 +1300

    Replace core session middlewere

--- src/Control/HybridSessionMiddleware.php
+++ src/Control/HybridSessionMiddleware.php
@@ -10,12 +10,20 @@ class HybridSessionMiddleware implements HTTPMiddleware
 {
     public function process(HTTPRequest $request, callable $delegate)
     {
-        // @todo
-        //
-        // if (HybridSession::is_enabled()) {
-        //    session_write_close();
-        // }
+        try {
+            // Start session and execute
+            $request->getSession()->init($request);
 
-        return $delegate($request);
+            // Generate output
+            return $delegate($request);
+        } finally {
+            // Save session data, even if there was an exception after the
+            // response has been returned.
+
+            // Note that save() will start/resume the session if required.
+            $request->getSession()->save($request);
+
+            session_write_close();
+        }
     }
 }
