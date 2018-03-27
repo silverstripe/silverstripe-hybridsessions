0eb64522570b638c7717ba95ebad2f343b6aacd3
Daniel Hensby <git@hens.by>
2018-03-27 11:15:00 +0100

    Skip tests that rely on DB if DB is not MySQL

--- tests/DatabaseStoreTest.php
+++ tests/DatabaseStoreTest.php
@@ -2,11 +2,22 @@
 
 namespace SilverStripe\HybridSessions\Tests;
 
+use SilverStripe\ORM\DB;
+use SilverStripe\ORM\Connect\MySQLDatabase;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\HybridSessions\Store\DatabaseStore;
 
 class DatabaseStoreTest extends AbstractTest
 {
+    protected function setUp()
+    {
+        parent::setUp();
+
+        if (!DB::get_conn() instanceof MySQLDatabase) {
+            $this->markTestSkipped('Only MySQL databases are supported');
+        }
+    }
+
     protected function getStore()
     {
         $store = Injector::inst()->get(DatabaseStore::class);
--- tests/HybridSessionTest.php
+++ tests/HybridSessionTest.php
@@ -2,13 +2,30 @@
 
 namespace SilverStripe\HybridSessions\Tests;
 
+use SilverStripe\ORM\DB;
+use SilverStripe\ORM\Connect\MySQLDatabase;
 use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Core\Config\Config;
 use SilverStripe\Core\TempFolder;
 use SilverStripe\HybridSessions\Tests\AbstractTest;
 use SilverStripe\HybridSessions\HybridSession;
 
 class HybridSessionTest extends AbstractTest
 {
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        if (!DB::get_conn() instanceof MySQLDatabase) {
+            // we can't always use the DB driver, so remove it if we aren't running a MySQL DB
+            Config::modify()->set(HybridSession::class, 'dependencies', [
+                'handlers' => [
+                    '%$\SilverStripe\HybridSessions\Store\CookieStore',
+                ],
+            ]);
+        }
+    }
 
     /**
      * @return HybridSessionStore_Cookie
